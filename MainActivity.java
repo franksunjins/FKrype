59b231c5fe4ae2f1123259773cdb05da1c77c502
franksunjin <franksunjin@gmail.com>
2013-09-24 21:38:48 -0400

    Update MainActivity.java
    Fix some fatal BUG

--- MainActivity.java
+++ MainActivity.java
@@ -35,19 +35,11 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//create input and password file
-		
-		try{
-			File dir = new File (root.getAbsolutePath() + "/FKrype");
-			dir.mkdirs();
-			FileOutputStream initPassword=new FileOutputStream(new File(dir, inputPassword));
-			OutputStreamWriter wr=new OutputStreamWriter(initPassword);
-			wr.write("input password");
-			wr.flush();
-			initPassword=new FileOutputStream(new File(dir, inputfile));
-			wr=new OutputStreamWriter(initPassword);
-			wr.write("franksunjin you are a fucking idiot input password");
-			wr.close();
-		}catch(Exception e){System.out.println(e.getStackTrace().toString());}
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("WARNING");
+		builder.setMessage(R.string.warningmessage);
+		builder.show();
+		//File dir = new File (root.getAbsolutePath() + "/FKrype");
 //		File file = new File(getString(R.string.dir), inputfile);
 		
 	}
@@ -64,11 +56,20 @@ public class MainActivity extends Activity {
 	        case R.id.item1:
 	            openQuit();
 	            return true;
+	        case R.id.item2:
+	            openHelp();
+	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
-
+	private void openHelp(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Help");
+		builder.setMessage(R.string.helpmessage);
+		builder.show();
+	}
+	
 	private void openQuit() {
 		finish();	
 	}
@@ -80,12 +81,13 @@ public class MainActivity extends Activity {
 	}
 	public void fEncrype(View view)
 	throws Exception{
-		File dir = new File (root.getAbsolutePath() + "/FKrype");
+		//File dir = new File (root.getAbsolutePath() + "/FKrype");
 		//File input, eoutput,doutput;
 		String textPassword="111111";
 		//EditText e1=((EditText)findViewById(R.id.editText1));
 		TextView t2=((TextView)findViewById(R.id.TextView01));
 		TextView t1=((TextView)findViewById(R.id.textView1));
+		if(t1.getText()=="")return;
 		inputfile=t1.getHint().toString();
 		RadioButton r2=(RadioButton)findViewById(R.id.radio21);
 		byte[] dataBytes = new byte[1024];
@@ -99,13 +101,14 @@ public class MainActivity extends Activity {
 			textPassword=((EditText)findViewById(R.id.editText1)).getText().toString();
 			dataBytes=AES.static_stringToByteArray(textPassword);
         	md.update(dataBytes, 0, dataBytes.length);
-		}else if(t2.getHint().toString()=="Open a file"){//using password file
-			inputPassword=t2.getHint().toString();
+		}else if(t2.getHint().toString()!="Password file"){//using password file
+			
+		try{inputPassword=t2.getHint().toString();
         FileInputStream fis = new FileInputStream(inputPassword);//password file
         while ((nread = fis.read(dataBytes)) != -1) {
         		md.update(dataBytes, 0, nread);
         }
-        fis.close();}
+        fis.close();}catch (Exception e){}}else return;
         byte[] mdbytes = md.digest();
         
         data  = new Scanner(new File(inputfile)).useDelimiter("\\Z").next();
@@ -114,25 +117,96 @@ public class MainActivity extends Activity {
         output=a.Encrypt(data);
         //String de=a.Decrypt(output);
         try{
-        	FileOutputStream initPassword=new FileOutputStream(new File(dir+"/", "encrypted.txt"));
+        	RadioButton r1=(RadioButton)findViewById(R.id.radio0);
+        	if(!r1.isChecked()){
+        	FileOutputStream initPassword=new FileOutputStream(new File(new File(inputfile).getParent()+"/", "encrypted.txt"));
 			OutputStreamWriter wr=new OutputStreamWriter(initPassword);
 			wr.write(output);
 			wr.flush();
-			/*
-			initPassword=new FileOutputStream(new File(dir+"/", "decrypted.txt"));
-			wr=new OutputStreamWriter(initPassword);
-			wr.write(de);
-			wr.flush();
-			*/
 			wr.close();
+			}else{
+				FileOutputStream initPassword=new FileOutputStream(new File(inputfile));
+				OutputStreamWriter wr=new OutputStreamWriter(initPassword);
+				wr.write(output);
+				wr.flush();
+				wr.close();
+			}
     	}catch(Exception e){e.printStackTrace();}
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, "Encrypte compelet", duration);
         toast.show();
+        //Scanner input = new Scanner(new File(filesDir, filename));
         }
 	
-	
+	public void fDecrype(View view)
+			throws Exception{
+				
+				//File dir = new File (root.getAbsolutePath() + "/FKrype");
+				//File input, eoutput,doutput;
+				String textPassword="111111";
+				//EditText e1=((EditText)findViewById(R.id.editText1));
+				TextView t2=((TextView)findViewById(R.id.TextView01));
+				TextView t1=((TextView)findViewById(R.id.textView1));
+				if(t1.getText()=="")return;
+				inputfile=t1.getHint().toString();
+				RadioButton r2=(RadioButton)findViewById(R.id.radio21);
+				byte[] dataBytes = new byte[1024];
+				 //       byte[] cB = new byte[1024];
+				int nread = 0; 
+				AES a=new AES();
+				String data;
+				String output;
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+				if(r2.isChecked()){//using text password
+					textPassword=((EditText)findViewById(R.id.editText1)).getText().toString();
+					dataBytes=AES.static_stringToByteArray(textPassword);
+		        	md.update(dataBytes, 0, dataBytes.length);
+				}else if(t2.getHint().toString()!="Password file"){//using password file
+					try{inputPassword=t2.getHint().toString();
+		        FileInputStream fis = new FileInputStream(inputPassword);//password file
+		        while ((nread = fis.read(dataBytes)) != -1) {
+		        		md.update(dataBytes, 0, nread);
+		        } 
+		        fis.close();
+					}catch(Exception e){}}else return;
+		        byte[] mdbytes = md.digest();
+		        
+		        data  = new Scanner(new File(inputfile)).useDelimiter("\\Z").next();
+		        a.setKey(mdbytes);
+		        //AES.self_test(mdbytes, "franksunjin111111112321juindsja", "franksunjin111111112321juindsja", 1);
+		        output=a.Decrypt(data);
+		        //String de=a.Decrypt(output);
+		        try{
+		        	RadioButton r1=(RadioButton)findViewById(R.id.radio0);
+		        	if(!r1.isChecked()){
+		            FileOutputStream initPassword=new FileOutputStream(new File(new File(inputfile).getParent()+"/", "decrypted.txt"));
+					OutputStreamWriter wr=new OutputStreamWriter(initPassword);
+					wr.write(output);
+					wr.flush();
+					wr.close();
+					}else{
+						FileOutputStream initPassword=new FileOutputStream(new File(inputfile));
+						OutputStreamWriter wr=new OutputStreamWriter(initPassword);
+						wr.write(output);
+						wr.flush();
+						wr.close();
+					}
+					/*
+					initPassword=new FileOutputStream(new File(dir+"/", "decrypted.txt"));
+					wr=new OutputStreamWriter(initPassword);
+					wr.write(de);
+					wr.flush();
+					*/
+					
+		    	}catch(Exception e){e.printStackTrace();}
+		        Context context = getApplicationContext();
+		        int duration = Toast.LENGTH_SHORT;
+		        Toast toast = Toast.makeText(context, "Decrypte complete", duration);
+		        toast.show();
+		        //@SuppressWarnings("unused")
+				//Scanner input = new Scanner(new File(dir+"/", "decrypted.txt"));
+		        }
 	
 //	private static final String FTYPE = ".txt";    
 	private void loadFileList() {
@@ -203,6 +277,7 @@ public class MainActivity extends Activity {
 		mPath = new File(root.getAbsolutePath());
 		TextView text1 = (TextView) findViewById(R.id.textView1);
 		text1.setText("");
+		text1.setHint("");
 		EditText e1=(EditText)findViewById(R.id.editText1);
 		e1.setText("");
 	}
